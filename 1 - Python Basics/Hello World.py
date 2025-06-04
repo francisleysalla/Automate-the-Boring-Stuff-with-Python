d3851a825ec80b06a82c7536eccef456890da9fa
Francisley Salla <sallafrancisley@gmail.com>
2025-06-03 21:29:12 -0300

    Create Hello World.py
    Starting the book.

---
+++ 1 - Python Basics/Hello World.py
@@ -0,0 +1,8 @@
+# This code says hello and asks for my name.
+
+print('Hello, world!')
+name = input('What is your name?')
+print("It is good to meet you, " + name)
+print(f"The length of your name is: {len(name)}")
+age = input("What's yor age?")
+print(f"You will be {str(int(age) +1)} in a year.")
